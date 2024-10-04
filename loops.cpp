10873102c48ef5f330ec158aa360dcad8ed6318f
Hritik Shokeen <76158614+Hritik-cyber@users.noreply.github.com>
2024-10-04 21:34:23 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Printing 'n' natural numbers
+    // int n;
+    // cout<<"Enter the value of n: ";
+    // cin>>n;
+
+    // for (int i=1; i<=n; ){
+
+    //     cout<<i<<" ";
+    //     i++;
+
+
+
+
+
+
+
+    // }
+
+    //sum of n natural numbers
+
+    // int n;
+    // cout<<"get the sum of first n natural numbers: ";
+    // cin>>n;
+    // int sum=0;
+    // for (int i=0; i<=n;i++){
+
+    //     sum=sum+i;
+
+
+    // }
+    // cout<<sum;
+
+    //printin the square pattern
+
+    // for (int i=1;i<=4;i++){
+    //     for (int j=1; j<=4;j++){
+    //         cout<<"*";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+    //printing numbers from n to 1
+
+    // int n;
+    // cout<<"enter the value of n: ";
+    // cin>>n;
+
+    // for (int i=n; i>=1;){
+    //     cout<<i<<" ";
+    //     i=i-1;
+
+    // }
+
+    //printing the sum of digits of a number
+
+    // int n=123;
+    // int sum=0;
+
+    // while (n>0){
+
+    //     sum=sum+ (n%10);
+    //     n=n/10;
+
+
+
+    // }
+    // cout<<sum;
+
+
+
+    //printing the sum of odd digits of a number 
+
+    // int n=123456;
+
+    // int sum=0;
+
+    // while (n>0){
+
+    //     int last_digit=n%10;
+    //     if (last_digit%2!=0){
+    //         sum=sum+last_digit;
+    //         cout<<last_digit<<" ";
+
+    //     }
+    //     n=n/10;
+
+    // }
+    // cout<<endl;
+
+    // cout<<sum;
+
+
+
+    //printing the reverse number and print the result
+
+
+    // int num=12345;
+    // int revnum=0;
+
+    // while (num>0){
+    //     int lastdigit;
+
+    //     lastdigit=num%10;
+    //     revnum=(revnum*10)+lastdigit;
+    //     num=num/10;
+
+
+
+    // }
+    // cout<<"reverse number: "<<revnum;
+
+
+    // checking if a number is prime or not
+
+    // int n;
+    // cout<<"check if the number is prime or not: ";
+    // cin>>n;
+
+    // int i=2;
+    // bool prime=true;
+    // while (i<n){
+    //     if (n%i==0){
+    //         prime=false;
+    //         break;
+
+    //     }
+    //     i++;
+        
+
+    //     }
+    
+    // cout<<prime;
+
+    //write a progama where user keep entering numbers till the number is a multiple of 10;
+
+    // while (true){
+
+    //     int n;
+    //     cout<<"enter your number: ";
+    //     cin>>n;
+
+    //     if (n%10==0){
+    //         break;
+    //     }
+
+
+    // }
+
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=90, b=78;
+    return 0;
+    cout<<a<<b;
+
+}
