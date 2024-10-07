179dacabaf24df3cab1a9937f2bb85fbfe2b24dc
Hritik Shokeen <76158614+HritikShokeen@users.noreply.github.com>
2024-10-07 21:52:47 +0530

    Add files via upload

---
+++ pointers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+void change(int a ){
+    a=20;
+    cout<<a;
+
+}
+
+int main(){
+    // char b='A';
+    // cout<<&b;
+    // char *ptr;
+    // cout<<ptr<<" "<<&ptr<<"\n";
+
+    // cout<<sizeof(ptr);
+
+    // ptr=&b;
+    // cout<<"\n"<<*ptr<<" "<<*(&b);
+    // int a=10;
+    // change(a);
+    // cout<<a;
+
+   cout<<int('A');
+   cout<<char(97);
+
+    
+
+
+    return 0;
+}
